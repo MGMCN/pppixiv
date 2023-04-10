9e1783720fe6ceb779244c17148552c60c5044cf
RIO98 <limingyang89@gmail.com>
2023-04-10 19:42:05 +0900

    getIllustListByUid(), getIllustRanking() added

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import requests
 
-# res = requests.post("http://127.0.0.1:3333/getIllustListByUid", data={"uid": "4837211"})
-res = requests.get("http://127.0.0.1:3333/getIllustListByUid")
+res = requests.post("http://127.0.0.1:3333/getIllustListByUid", data={"uid": "4837211"})
 
 print(res.text)
