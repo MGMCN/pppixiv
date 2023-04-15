1aead6630e1f45d962cf884bd205ff9bc40a010d
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-04-15 14:36:04 +0900

    refactoring app.py

--- app.py
+++ app.py
@@ -7,31 +7,22 @@ from flask import Flask
 from router.pixiv_router import pixiv_router, router_set_pixiv_api
 from services.pixiv import Pixiv
 
-load_dotenv(verbose=True)
 
-app = Flask(__name__)
-app.logger.setLevel(logging.DEBUG)
+class App(Flask):
+    def __init__(self, name):
+        super().__init__(name)
+        self.init()
 
-username = os.getenv("username")
-password = os.getenv("password")
-myPixiv = Pixiv(service_name="pixiv", username=username, password=password, interval=3500)
-myPixiv.set_logger(app.logger)
+    def init(self):
+        load_dotenv(verbose=True)
+        self.logger.setLevel(logging.DEBUG)
+        self.username = os.getenv("username")
+        self.password = os.getenv("password")
+        self.myPixiv = Pixiv(service_name="pixiv", username=self.username, password=self.password, interval=3500)
+        self.register_blueprint(pixiv_router)
+        self.pass_context()
 
-app.register_blueprint(pixiv_router)
-
-
-# Only to see if this server is running correctly
-@app.route('/')
-def lsp():
-    return 'Hello World! lsp!'
-
-
-if __name__ == '__main__':
-    # Not graceful
-    router_set_pixiv_api(myPixiv)
-    app.logger.debug("Authorized on pixiv account %s. Please wait for authentication.", username)
-    success = myPixiv.start_pixiv_session()
-    if success:
-        app.logger.debug("Pixiv Login Complete.")
-        app.logger.debug("=================================================================")
-        app.run(host='0.0.0.0', port=5000)
+    def pass_context(self):
+        self.myPixiv.set_logger(self.logger)
+        # Not graceful
+        router_set_pixiv_api(self.myPixiv)
